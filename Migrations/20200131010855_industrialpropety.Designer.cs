9f6e587993b299093d8fb173eeee882b79f3d055
Mr. Simi <adegokesimi@gmail.com>
2020-01-30 17:20:36 -0800

    migrations to database

--- Migrations/20200131010855_industrialpropety.Designer.cs
+++ Migrations/20200131010855_industrialpropety.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using tinder4apartment.Data;
 
 namespace tinder4apartment.Migrations
 {
     [DbContext(typeof(PropertyDbContext))]
-    partial class PropertyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200131010855_industrialpropety")]
+    partial class industrialpropety
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
