bb2e384a7883e51010695af4b2c98a1d5881d0f2
Mr. Simi <adegokesimi@gmail.com>
2020-02-16 16:05:46 +0100

    adding other files

---
+++ Migrations/20200215161052_updatedtoonlineserver.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace tinder4apartment.Migrations
+{
+    public partial class updatedtoonlineserver : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
